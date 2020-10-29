7283ee88b3a33c47710d36f76e19e52ac2579f38
Emanuel Zaymus <emanuel.zaymus@prosoft.sk>
2020-10-29 09:29:47 +0100

    Fuzzy tests - find

--- DataStructures.Tests/KDTreeFuzzyTests.cs
+++ DataStructures.Tests/KDTreeFuzzyTests.cs
@@ -63,8 +63,19 @@ namespace DataStructures.Tests
             var tree = new KDTree<Town>(towns, Town.GetComparers());
             var checkList = towns.ToList();
 
-            for (int i = 0; i < 100000; i++)
+            int i;
+            for (i = 0; i < 1000 && checkList.Count > 0; i++)
             {
+                var town = checkList[r.Next(checkList.Count)];
+                int townCount = checkList.Count(x => x.Equals(town));
+                var foundTowns = tree.Find(town);
+                Assert.AreEqual(town, foundTowns.First());
+                Assert.AreEqual(townCount, foundTowns.Count);
+                var countRemoved = tree.Remove(town);
+                Assert.AreEqual(townCount, countRemoved);
+                checkList.RemoveAll(x => x.Equals(town));
+                CollectionAssert.AreEquivalent(checkList, tree.ToList());
+
                 var pos = r.NextDouble() < 0.1 ? RandomDataGenerator.GenerateTownPosition() : towns[r.Next(count)];
                 var found = tree.FindAt(pos);
                 tree.RemoveAt(pos);
@@ -84,7 +95,7 @@ namespace DataStructures.Tests
         [Timeout(30000)]
         public void KDTree_Town_Fuzzing()
         {
-            Random r = new Random(9876);
+            Random r = new Random(23);
             var tree = new KDTree<Town>(Town.GetComparers());
             int initCount = 10000;
             var towns = RandomDataGenerator.GenerateTowns(initCount).ToArray();
@@ -92,21 +103,48 @@ namespace DataStructures.Tests
             var checkList = new List<Town>(Convert.ToInt32(initCount * 1.5));
             checkList.AddRange(towns);
 
-            for (int i = 0; i < 100000; i++)
+            int i;
+            for (i = 0; i < 40000; i++)
             {
-                switch (r.Next(2))
+                switch (r.Next(3))
                 {
                     case 0:
-                        Town t = RandomDataGenerator.GenerateTown();
-                        tree.Add(t);
-                        checkList.Add(t);
+                        if (r.Next(2) == 0)
+                        {
+                            Town t = RandomDataGenerator.GenerateTown();
+                            tree.Add(t);
+                            checkList.Add(t);
+                        }
+                        else
+                        {
+                            var generTowns = RandomDataGenerator.GenerateTowns(r.Next(5, 15)).ToArray();
+                            tree.AddRange(generTowns);
+                            checkList.AddRange(generTowns);
+                        }
                         CollectionAssert.AreEquivalent(checkList, tree.ToList());
                         break;
                     case 1:
-                        TownPosition pos1 = RandomDataGenerator.GenerateTownPosition();
-                        TownPosition pos2 = RandomDataGenerator.GenerateTownPosition();
-                        var found = tree.FindRange(pos1, pos2);
-                        tree.RemoveRange(pos1, pos2);
+                        var town = checkList[r.Next(checkList.Count)];
+                        int townCount = checkList.Count(x => x.Equals(town));
+                        var foundTowns = tree.Find(town);
+                        Assert.AreEqual(town, foundTowns.First());
+                        Assert.AreEqual(townCount, foundTowns.Count);
+                        break;
+                    case 2:
+                        ICollection<Town> found;
+                        if (r.Next(2) == 0)
+                        {
+                            var pos = r.NextDouble() < 0.1 ? RandomDataGenerator.GenerateTownPosition() : checkList[r.Next(checkList.Count)];
+                            found = tree.FindAt(pos);
+                            tree.RemoveAt(pos);
+                        }
+                        else
+                        {
+                            TownPosition pos1 = RandomDataGenerator.GenerateTownPosition();
+                            TownPosition pos2 = RandomDataGenerator.GenerateTownPosition();
+                            found = tree.FindRange(pos1, pos2);
+                            tree.RemoveRange(pos1, pos2);
+                        }
                         checkList.RemoveAll(x => found.Contains(x));
                         CollectionAssert.AreEquivalent(checkList, tree.ToList());
                         break;
